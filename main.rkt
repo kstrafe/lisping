d0d6cc38642424d43d14066bcd8f5a5d71c8ab88
Bourgond Aries <macocio@gmail.com>
2016-09-19 23:28:07 +0200

    Slowly build up a reportoire

--- main.rkt
+++ main.rkt
@@ -1,5 +1,21 @@
 [module main racket
 
+[define-syntax [example1 syntax-tree]
+	[writeln [syntax->datum syntax-tree]]
+	[syntax (void)]]
+[example1 As many as you want]
+
+[define-syntax [example2 syntax-tree]
+	[writeln [syntax->datum syntax-tree]]
+	#'(void)]
+[example2 We can use a shorthand]
+
+[define-syntax [example3 syntax-tree]
+	[writeln [syntax->datum syntax-tree]]
+	#'(displayln "example 3")]
+[example3 We can use a shorthand]
+
+
 [define-syntax [alias from-to]
 	[define from-to-s [syntax->datum from-to]]
 	[writeln from-to-s]
@@ -9,9 +25,11 @@
 	[define new-name [cadr from-to-s]]
 	[define old-name [caddr from-to-s]]
 
-	[syntax [+ 1]]]
+	#'[syntax [define-syntax [new-name args]
+		[+ 1]]]
+	#'[syntax [define-syntax]]]
 
-[alias macro define-syntax-rule]
+[syntax->datum [alias macro define-syntax-rule]]
 
 [exit]
 
