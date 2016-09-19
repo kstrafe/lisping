8aab1fe12886ab78e378d1911167b4f3da9744e9
Bourgond Aries <macocio@gmail.com>
2016-09-20 00:21:15 +0200

    Add a bunch of matches and other features

--- main.rkt
+++ main.rkt
@@ -1,6 +1,11 @@
 [module main racket
 
+[require [for-syntax racket/match]]
+
+[displayln "ENTERING RUNTIME"]
+
 [define-syntax [example1 syntax-tree]
+	[displayln "ENTERING COMPILE-TIME"]
 	[writeln [syntax->datum syntax-tree]]
 	[syntax (void)]]
 [example1 As many as you want]
@@ -15,22 +20,39 @@
 	#'(displayln "example 3")]
 [example3 We can use a shorthand]
 
+[define stx
+	#'[when [> x 1] [100 [displayln 'kek]]]]
 
-[define-syntax [alias from-to]
-	[define from-to-s [syntax->datum from-to]]
-	[writeln from-to-s]
-	[writeln [cadr from-to-s]]
-	[writeln [caddr from-to-s]]
+stx
+[syntax->datum stx]
+[syntax-source stx]
+[syntax-line   stx]
+[syntax-column stx]
+[syntax-e      stx]  ; convert only a top level
 
-	[define new-name [cadr from-to-s]]
-	[define old-name [caddr from-to-s]]
+[define-syntax [reverser stx]
+	[datum->syntax stx [reverse [cdr [syntax->datum stx]]]]]
+[reverser "backwards" "am" "i" list]
 
-	#'[syntax [define-syntax [new-name args]
-		[+ 1]]]
-	#'[syntax [define-syntax]]]
+; Using match on the syntax
+[define-syntax [custom-if stx]
+	; Can also use syntax->list here.
+	; That preserves some information
+	[match (syntax->datum stx)
+		[(list _ condition true-expr false-expr)
+		 (datum->syntax stx `(cond [,condition ,true-expr]
+		                           [else ,false-expr]))]]]
+[custom-if [> 15 10] "Bigger" "Smaller"]
 
-[syntax->datum [alias macro define-syntax-rule]]
+; Creating compile-time callable functions/variables
+[begin-for-syntax
+	[define a-constant-value "This is my compile constant"]]
+[define-for-syntax value1 " | Another one"]
+[define-syntax [example4 stx]
+	[datum->syntax stx [string-append a-constant-value value1]]]
+[example4]
+; a-constant-value ; This won't work during runtime
 
-[exit]
+
 
 ]
