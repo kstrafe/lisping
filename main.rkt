adfe4615d619426ef32a1ad6ad10f5c68c95c75f
Bourgond Aries <macocio@gmail.com>
2016-09-22 03:33:54 +0200

    Toy with writing to a file

--- main.rkt
+++ main.rkt
@@ -83,4 +83,60 @@ stx
 
 [replace-first '[we are warriors] 'i]
 
+[define [my-length my-list]
+	[match my-list
+		[(list) 0]
+		[else [+ 1 [my-length [cdr my-list]]]]]]
+
+[my-length '[1 2 3 3 3 2]]
+
+[define [take my-list count]
+	[if [= count 0]
+		'()
+		[match my-list
+			['() '()]
+			[else [cons [car my-list] [take [cdr my-list] [- count 1]]]]]]]
+
+[take '[1 2 3 4 5] 2]
+
+[define [drop my-list count]
+	[if [= count 0]
+		my-list
+		[match my-list
+			['() '()]
+			[else [drop [cdr my-list] [- count 1]]]]]]
+
+[drop '[1 2 3 4 5 6 7] 5]
+
+[define [my-append list1 list2]
+	[match list1
+		['() list2]
+		[else [cons [car list1] [my-append [cdr list1] list2]]]]]
+
+[my-append '[1 2] '[3 9]]
+
+[define [my-member list1 element]
+	[match list1
+		['() #f]
+		[else [if [= [car list1] element]
+			#t
+			[my-member [cdr list1] element]]]]]
+
+[my-member '[1 8 3] 1]
+
+[define [my-position list1 element]
+	[match list1
+		['() 0]
+		[else
+			[if [= [car list1] element]
+				0
+				[+ 1 [my-position [cdr list1] element]]]]]]
+
+[my-position '[1 2 3 8 9 0] 3]
+
+[define dir [find-system-path 'temp-dir]]
+[with-output-to-file [build-path dir "stuff.txt"]
+	[lambda () [print "TOP KEK"]]
+	#:exists 'replace]
+
 ]
