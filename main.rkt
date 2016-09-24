3e6a5c9c116727aa0bce6398a10f9fc484cf60fa
Bourgond Aries <macocio@gmail.com>
2016-09-24 10:09:43 +0200

    Identified the culprit

--- main.rkt
+++ main.rkt
@@ -2,6 +2,17 @@
 
 (require opengl opengl/util ffi/vector racket/gui)
 
+[define [square x y scale]
+	[glBegin GL_TRIANGLES]
+	[glColor3f 1.0 1.0 1.0]
+	[glVertex3d x y 0.0]
+	[glVertex3d [+ x scale] y 0.0]
+	[glVertex3d x [+ y scale] 0.0]
+	[glVertex3d x [+ y scale] 0.0]
+	[glVertex3d [+ x scale] [+ y scale] 0.0]
+	[glVertex3d [+ x scale] y 0.0]
+	[glEnd]]
+
 (define (resize w h)
   (glViewport 0 0 w h))
 
@@ -17,7 +28,6 @@
   (glMatrixMode GL_MODELVIEW)
   (glLoadIdentity)
 
-  #|
   (glBegin GL_TRIANGLES)
   (glColor3f 1.0 0.0 0.0)
   (glVertex3d 0.25 0.25 0.0)
@@ -25,13 +35,10 @@
   (glVertex3d 0.75 0.25 0.0)
   (glColor3f 0.0 0.0 1.0)
   (glVertex3d 0.75 0.75 0.0)
+	[displayln "We were retarded"]
   (glEnd)
-  |#
-  [glEnableVertexAttribArray 0]
-  [glBindBuffer GL_ARRAY_BUFFER bufn]
-  [glVertexAttribPointer 0 3 GL_FLOAT #f 0 0]
-  ;[glDrawArrays GL_TRIANGLES 0 3]
-  [glDisableVertexAttribArray 0])
+
+  [square 0.45 0.45 0.1])
 
 
 (define my-canvas%
@@ -41,6 +48,10 @@
    (define/override (on-paint)
       (with-gl-context
         (lambda ()
+					[define shader [glCreateShader GL_VERTEX_SHADER]]
+					[define shader2 [glCreateShader GL_FRAGMENT_SHADER]]
+					[displayln shader]
+					[displayln shader2]
           (draw-opengl)
           (swap-gl-buffers))))
 
@@ -55,45 +66,8 @@
 
 (define win (new frame% (label "Racket Rosetta Code OpenGL example") (min-width 200) (min-height 200)))
 
-(define gl  (new my-canvas% (parent win)))
+(define gl (new my-canvas% (parent win)))
 
-[define triangle '[-1.0 -1.0 0.0
-                    1.0 -1.0 0.0
-                    0.0  1.0 0.0]]
-
-[define f32triangle [list->f32vector triangle]]
-[define buf [glGenBuffers 1]]
-[define bufn [first [u32vector->list buf]]]
-[glBindBuffer GL_ARRAY_BUFFER bufn]
-[define-values [ty ptr] [gl-vector->type/cpointer f32triangle]]
-[glBufferData GL_ARRAY_BUFFER [length triangle] ptr GL_STATIC_DRAW]
-
-[glCreateProgram]
-[define vshad [glCreateShader GL_VERTEX_SHADER]]
-[define vshad2 [glCreateShader GL_VERTEX_SHADER]]
-[define fshad [glCreateShader GL_FRAGMENT_SHADER]]
-
-[printf "vshad: ~a\n" vshad]
-[printf "vshad2: ~a\n" vshad2]
-[printf "fshad ~a\n" fshad]
-
-[define vshad-t [file->string "vertex.glsl"]]
-[define fshad-t [file->string "fragment.glsl"]]
-
-[glShaderSource  vshad 1 [vector vshad-t] [s32vector [string-length vshad-t]]]
-[glCompileShader vshad]
-
-[define compstat [glGetShaderiv vshad GL_COMPILE_STATUS]]
-[printf "compstat: ~a\n" compstat]
-
-[when [not [= compstat 0]]
-  [define logl [glGetShaderiv vshad GL_INFO_LOG_LENGTH]]
-  [printf "logl: ~a\n" logl]
-  [when [and [> logl 0] #t]
-    [displayln "KEKOS"]
-    [define-values [nums bts] [glGetShaderInfoLog vshad logl]]
-    [displayln bts]
-    [displayln "EKKEK"]]]
 
 (send win show #t)
 
