b852f7b4b4b557a9dd5b76abde9426eed355459a
Bourgond Aries <macocio@gmail.com>
2016-09-24 05:06:57 +0200

    Get a buffer to work!

--- main.rkt
+++ main.rkt
@@ -1,12 +1,19 @@
 [module main racket
 
-(require opengl opengl/util racket/gui)
+[define current 0]
+[define [tic] [set! current [current-inexact-milliseconds]]]
+[define [toc] [displayln [- [current-inexact-milliseconds] current]]]
+
+[tic]
+(require opengl opengl/util ffi/vector racket/gui)
+[toc]
 
 (define (resize w h)
   (glViewport 0 0 w h))
 
 [define count 0]
 
+[tic]
 (define (draw-opengl)
 	[displayln count]
 	[set! count [add1 count]]
@@ -29,8 +36,9 @@
   (glColor3f 0.0 0.0 1.0)
   (glVertex3d 0.75 0.75 0.0)
   (glEnd))
+[toc]
 
-
+[tic]
 (define my-canvas%
   (class* canvas% ()
     (inherit with-gl-context swap-gl-buffers)
@@ -47,10 +55,27 @@
           (resize width height))))
 
     (super-instantiate () (style '(gl)))))
+[toc]
 
+[tic]
 (define win (new frame% (label "Racket Rosetta Code OpenGL example") (min-width 200) (min-height 200)))
+[toc]
+[tic]
 (define gl  (new my-canvas% (parent win)))
+[toc]
 
+[define triangle '[-1.0 -1.0 0.0
+                    1.0 -1.0 0.0
+									  0.0  1.0 0.0]]
+[define f32triangle [list->f32vector triangle]]
+[define buf [glGenBuffers 1]]
+[define bufn [first [u32vector->list buf]]]
+[glBindBuffer GL_ARRAY_BUFFER bufn]
+[define-values [ty ptr] [gl-vector->type/cpointer f32triangle]]
+[glBufferData GL_ARRAY_BUFFER [length triangle] ptr GL_STATIC_DRAW]
+
+[tic]
 (send win show #t)
+[toc]
 
 ]
