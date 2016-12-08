2eb75a9e525607244edb764b8097032358266f60
Kevin Robert Stravers <macocio@gmail.com>
2016-12-08 23:42:58 +0100

    Start namespacing and impl some other stuff

--- transpile.rkt
+++ transpile.rkt
@@ -4,6 +4,9 @@
 ; The idea is to use racket macros and functions to transpile racket to any language.
 ; The target language can be changed by swapping out the macros.
 
+(define-namespace-anchor a)
+(define ns (namespace-anchor->namespace a))
+
 [define (Option type)
   [string-append "Option<" type ">"]]
 
@@ -11,7 +14,10 @@
   [string-append "Result<" type ", " error ">"]]
 
 [define (array elements)
-  [string-append "[" [string-join (map ~a elements) ", "] "]"]]
+  [string-append "[" [commaify elements] "]"]]
+
+[define (commaify elements)
+  [string-join (map ~a elements) ", "]]
 
 [define-syntax-rule (Some element) [string-append "Some(" [symbol->string [quote element]] ")"]]
 
@@ -29,25 +35,53 @@
   [when (not (= (modulo bits 2) 0)) [raise "Number of bits not a power of 2"]]
   [string-append "u" [number->string bits]]]
 
-[define usize "usize"]
-[define isize "isize"]
-[define bool "bool"]
-[define char "char"]
+[define (ref type) [string-append "&" type]]
+[define (refmut type) [string-append "&mut " type]]
+[define (usize) "usize"]
+[define (isize) "isize"]
+[define (bool) "bool"]
+[define (char) "char"]
 [define (f bits)
   [when (> bits MAX_BITS_FLOAT) [raise "Too many bits for float type"]]
   [when (not (= (modulo bits 2) 0)) [raise "Number of bits not a power of 2"]]
   [string-append "f" [number->string bits]]]
 
+[define-syntax-rule (argument-list arguments)
+  [commaify [for/list ([i [quote arguments]])
+    [match i
+      [(list name type) [string-append [symbol->string name] ": " [eval type ns]]]]]]]
+
+[define-syntax-rule (fn name in out body)
+  [let ([arrow [if (= [string-length out] 0) "" " -> "]])
+    [string-append "fn " [symbol->string [quote name]] "(" [argument-list in] ")" arrow out " {\n" [string-join [for/list ([i [quote body]]) [eval i ns]] ";\n"] "\n}"]]]
+
 ; Your code
 [displayln [Option [i 128]]]
 [displayln [Option [f 64]]]
 [displayln [Result [f 64] [i 32]]]
-[define-syntax-rule (var name expr) [string-append "let mut " [symbol->string [quote name]] " = " expr ";"]]
-[define-syntax-rule (val name expr) [string-append "let " [symbol->string [quote name]] " = " expr ";"]]
+[define-syntax-rule (var name expr) [string-append "let mut " [symbol->string [quote name]] " = " expr]]
+[define-syntax-rule (val name expr) [string-append "let " [symbol->string [quote name]] " = " expr]]
 
 ; [fn main '[] [Option [i 16]] [Result [i 32] [i 32]]]
 ; fn - Name, arguments, output, body
 [displayln [array '[1 2 3]]]
 [displayln [var a [array '[1 2 3]]]]
 [displayln [val b [array '[1 2 3]]]]
-[displayln [val c [Some 3]]]
+
+[define-syntax-rule (dep name version)
+  [let [(name [symbol->string [quote name]])
+      (version [symbol->string [quote version]])]
+    [displayln [string-append name " = \"" version "\""]]
+    [string-append "extern crate " name ";"]]]
+
+[define-syntax-rule (+= left right)
+  [string-append [symbol->string [quote left]] " += " right]]
+
+[dep slog-json 1.3.2]
+[dep bgjk 0.1.0]
+[fn main ([in [i 32]] [control [refmut [char]]]) [Option [i 32]] ""]
+[display [fn main [] "" [
+  [val a "kek"]
+  [+= kek "rek"]
+  [var b "one hundred"]]]]
+
