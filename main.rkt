3d7239e77c342456da70dc77da9f6892975beb6e
Kevin Robert Stravers <macocio@gmail.com>
2016-12-19 22:57:04 +0100

    Implement the y-collision system

--- main.rkt
+++ main.rkt
@@ -1,263 +1,156 @@
 #! /usr/bin/env racket
 #lang racket
 
-[require racket/serialize racket/gui ffi/vector math/matrix opengl opengl/util finalizer]
-[require [for-syntax racket/list]]
-[require "util.rkt" "logger.rkt"]
+[require rackunit]
+[require sugar]
 
-[serializable-struct Point (x y) #:transparent #:guard
-  [lambda (x y name)
-    [values
-      [real->single-flonum x]
-      [real->single-flonum y]]]]
+[define (nth list n)
+  [if [= n 0]
+    [car list]
+    [nth [cdr list] [sub1 n]]]]
 
-[define foothold%
-  [class object% [init start stop]
-    [define point-1 start]
-    [define point-2 stop]
-    [define-values (left right) [values empty empty]]
-    [unless [< [Point-x point-1] [Point-x point-2]]
-      [set! point-1 stop]
-      [set! point-2 start]]
-    [super-new]
-    [define/public (map-x-to-y x)
-      [let ([x [real->single-flonum x]])
-        [if [and [>= x [Point-x point-1]] [<= x [Point-x point-2]]]
-          [+ [Point-y point-1]
-             [* [- x [Point-x point-1]] [get-slope]]]
-          [if [> x [Point-x point-2]]
-            'right
-            'left]]]]
-    [define/public (left-most-x)
-      [Point-x point-1]]
-    [define/public (right-most-x)
-      [Point-x point-2]]
-    [define/public (get-slope)
-      [let ([dx [- [Point-x point-2] [Point-x point-1]]]
-            [dy [- [Point-y point-2] [Point-y point-1]]])
-        [/ dy dx]]]]]
-
-[define footholds%
-  [class object%
-    [define footholds #[]]
-    [define lefts [make-hash]]
-    [define rights [make-hash]]
-    [super-new]
-    [define/public (add-foothold fh)
-      [set! footholds [vector-append footholds [vector fh]]]
-      [sub1 [vector-length footholds]]]
-    [define/public (connect-footholds left-index right-index)
-      [hash-set! lefts right-index left-index]
-      [hash-set! rights left-index right-index]]
-    [define/public (get-footholds)
-      [info 'lefts: lefts 'rights: rights]
-      footholds]
-    [define/public (get-foothold index)
-      [if [and [>= index 0] [< index [vector-length footholds]]]
-        [vector-ref footholds index]
+[struct Point (x y) #:prefab]
+[define [find-between comparator elements]
+  [match elements
+    [(list left right _ ...)
+      [if [comparator left right]
+        [list left right]
+        [find-between comparator [cdr elements]]]]
+    [(list left right)
+      [if [comparator left right]
+        [list left right]
         empty]]
-    [define/public (find-foothold-below point)
-      [let ([my-x [Point-x point]]
-            [my-y [Point-y point]]
-            [highest -inf.f]
-            [current empty]
-            [index -1]
-            [current-index empty])
-        [for ([fh footholds])
-          [set! index [add1 index]]
-          [when [and [>= my-x [send fh left-most-x]]
-                     [<= my-x [send fh right-most-x]]]
-            [let ([fh-y [send fh map-x-to-y my-x]])
-              [when [and [>= my-y fh-y]
-                         [> fh-y highest]]
-                [set! current fh]
-                [set! current-index index]
-                [set! highest fh-y]]]]]
-        [values current current-index]]]]]
+    [else empty]]]
+[check-equal? '[6 8]
+   [find-between [lambda (left right) [< left 7 right]] '[1 2 3 6 8 9 10]]
+   "Find the numbers surrounding a test number in a sequence"]
+[define (get-slope left right)
+  [/ [- [Point-y right] [Point-y left]] [- [Point-x right] [Point-x left]]]]
+[define (map-x-to-y left right x)
+  [+ [Point-y left] [* [- x [Point-x left]] [get-slope left right]]]]
+[define [find-between-points x-pos elements]
+  [find-between [lambda (left right) [<= [Point-x left] x-pos [Point-x right]]]
+    elements]]
+[define (adjust-height x foothold-group)
+  [let ([points [find-between-points x foothold-group]])
+    [if [empty? points]
+      empty
+      [map-x-to-y [car points] [cadr points] x]]]]
+[define (list-to-points list)
+  [map [lambda (x) [Point [car x] [cadr x]]] list]]
+[define (semi-circle-foothold start-x start-y)
+  [for/list ([x-value [range start-x [+ start-x pi] 1/100]])
+    [Point x-value [+ [sin [- x-value start-x]] start-y]]]]
+[define (semi-circle-map positions)
+  [for/list ([pos positions])
+    [semi-circle-foothold [car pos] [cadr pos]]]]
+[define (map-empty function iterable)
+  [map [lambda (x) [if [empty? x] empty [function x]]] iterable]]
+[define (find-y-foothold-collision x old-y new-y footholds)
+  [let* ([heights [map [lambda (foothold-group) [adjust-height x foothold-group]] footholds]]
+         [heights [map-empty [lambda (height) [if [< height old-y] height empty]] heights]]
+         [heights [map-empty [lambda (height) [if [< new-y height] height empty]] heights]]
+         [maxima  [values->list
+           [for/fold ([max-height -inf.f]
+                      [max-index empty])
+                     ([element heights]
+                      [index (in-naturals)])
+             [if [empty? element]
+               [values max-height max-index]
+               [if [> element max-height]
+                 [values element index]
+                 [values max-height max-index]]]]]])
+    [if [empty? [cadr maxima]]
+      [list new-y empty]
+      maxima]]]
+[define (resolve-y x old-y new-y foothold-index footholds)
+  [if [empty? foothold-index]
+    [find-y-foothold-collision x old-y new-y footholds]
+    [let* ([foothold-group [nth footholds foothold-index]]
+           [new-state [list [adjust-height x foothold-group] foothold-index]])
+      [if [empty? [car new-state]]
+        [resolve-y x old-y new-y '[] footholds]
+        new-state]]]]
+[define (semi-circle start-x start-y)
+  [for/list ([x-value [range start-x [+ start-x 2] 1/100]])
+    [list x-value [/ [+ x-value [sin [* 12 x-value]] start-y] 4]]]]
 
-[define-syntax (create-floor points)
-  [define vertices [cdr [syntax->datum points]]]
-  [define footholder-name [car vertices]]
-  [set! vertices [cdr vertices]]
-  [define count [sub1 [length vertices]]]
-  [define working-list empty]
-  [define current-x -inf.f]
-  [let loop ([working-vertices vertices])
-    [when [>= [length working-vertices] 2]
-      [let* ([start [cons 'Point [car working-vertices]]]
-            [stop [cons 'Point [cadr working-vertices]]]
-            [newed `[new foothold% [start ,start] [stop ,stop]]])
-        [if [> current-x [caar working-vertices]]
-          [raise-syntax-error 'Sorted-test-x "List of points must be sorted from left-to-right with respect to x"]
-          [set! current-x [caar working-vertices]]]
-        [if [empty? working-list]
-          [set! working-list [list newed]]
-          [set! working-list [cons newed working-list]]]
-        [loop [cdr working-vertices]]]]]
-  [define final [cons 'begin0 [for/list ([foothold [reverse working-list]])
-    [append `[send ,footholder-name add-foothold] [list foothold]]]]]
-  [set! final
-    `[let ([start-index ,final])
-      [for ([i [in-range start-index [sub1 [+ start-index ,count]]]])
-        [warn 'current-index i]
-        [send ,footholder-name connect-footholds i [add1 i]]]]]
-  [datum->syntax #'safe final #'srcloc]]
+[define m [semi-circle-map '[[0 0] [1 0] [2 0] [0 -2]]]]
 
-[define (pointify vertices)
-  [map [lambda (vertex)
-      [Point [car vertex] [cadr vertex]]]
-    vertices]]
-[define (sorted-on-x? points)
-  [apply < [map Point-x points]]]
-[sorted-on-x? [pointify '[[-4 0] [1 0] [2 0] [4 1]]]]
+; [define footholds '[[-1 0] [-0.5 -0.3] [0 0.2] [0.5 0.3] [1 0.1]]]
+[define footholds [semi-circle -1 0]]
+[define point-footholds [list [list-to-points footholds]]]
+;; The machinery needed to compute a new y value
+;; It takes the new x position, the old y value, and the new y value
+;; If it falls off a foothold, the new-y will be assumed. Else, the y value
+;; is interpolated from the foothold.
+;[resolve-y 0.2 1 -10.3 '[] m]
+;point-footholds
+;point-footholds
+
+[require ffi/vector finalizer math/matrix opengl opengl/util racket/generic racket/gui racket/serialize]
+[require [for-syntax "logger.rkt" racket/list]]
+[require "classes.rkt" "util.rkt" "logger.rkt" "window.rkt"]
+
+[define mychar '[[0 0] [0.2 0] [0.2 0.2] [0 0.2]]]
+
+[define-for-syntax [replace-first]
+  [let ([found #f])
+    [define [replacer exp old new]
+      [cond
+        [[null? exp] '[]]
+        [[not [pair? exp]]
+          [cond
+            [[and [eq? exp old] [not found]]
+              [set! found #t] new]
+            [else exp]]]
+        [else
+          [cons [replacer [car exp] old new]
+          [replacer [cdr exp] old new]]]]]
+    replacer]]
+
+[define-syntax (loop syn)
+  [let* ([elements [cdr [syntax->datum syn]]]
+         [names [for/list ([i [car elements]]) [car i]]]
+         [expression [cdr elements]]
+         [to-ret [[replace-first] expression 'game-loop [cons 'game-loop names]]]
+         [to-ret `[let game-loop ,[car elements] ,[cons 'begin to-ret]]])
+           [trce to-ret]
+           [datum->syntax syn to-ret]]]
+
+[define (clamp number range)
+  [if [> number range]
+    0
+    number]]
 
 
-[define player% [class object%
-  [init]
-  [define position [Point 0 1]]
-  [define current-foothold-index 0]
-  [define current-foothold empty]
-  [super-new]
-  [define/public (correct-y footholds)
-    [if [empty? current-foothold]
-      [set!-values (current-foothold current-foothold-index) [send footholds find-foothold-below position]]
-      [let ([potential-y [send current-foothold map-x-to-y [Point-x position]]])
-        [cond
-          [(number? potential-y) [set! position [Point [Point-x position] [Point-y position]]]]
-          [(eq? 'left potential-y)
-            [set!-values (current-foothold current-foothold-index) [values [send footholds get-foothold [sub1 current-foothold-index]] [sub1 current-foothold-index]]]]
-          [(eq? 'right potential-y)
-            [warn 'going-right 'new-index [add1 current-foothold-index]]
-            [set!-values (current-foothold current-foothold-index) [values [send footholds get-foothold [add1 current-foothold-index]] [add1 current-foothold-index]]]]
-          [else '[]]]]]]
-  [define/public (status)
-    [info 'position position 'fhi current-foothold-index]]
-  [define/public (move-in-x step)
-    [set! position [Point [+ [Point-x position] step] [Point-y position]]]]]]
-
-[define character [new player%]]
-
-[define fhs [new footholds%]]
-[create-floor fhs [-1 0] [1 0] [2 0] [4 1]]
-[create-floor fhs [-5 -1] [6 -1]]
-[send fhs get-footholds]
-
-;; Ideally we want to write stuff like
-[with-input-from-file "shape.rkt"
-  [lambda ()
-    [read]]]
-
-; [require racket/gui]
-; [require [lib "gl.ss" "sgl"] [lib "gl-vectors.ss" "sgl"]]
-
-[require racket/serialize racket/gui ffi/vector math/matrix opengl opengl/util]
-[require [for-syntax racket/list]]
-[require "logger.rkt"]
-[define an 0.0]
-[define [resize w h]
-  [glViewport 0 0 w h] #t]
-[define [draw-opengl]
-  [glClearColor 0.2 0.3 0.3 1.0]
-  [glPolygonMode GL_FRONT_AND_BACK GL_LINE]
-  [glClear [bitwise-ior GL_COLOR_BUFFER_BIT GL_DEPTH_BUFFER_BIT]]
-  [let [(mvp [glGetUniformLocation program "mvp"])]
-    [dbug mvp]
-    [set! an [+ an 0.01]]
-    [trce "x-translate" an]
-    [let [(translate
-      [matrix [[1.0 0.0 0.0 [/ [sin an] 2]]
-               [0.0 1.0 0.0 [/ [cos an] 2]]
-               [0.0 0.0 1.0 [/ [sin an] 2]]
-               [0.0 0.0 0.0 1.0]]])]
-      [let [(fac [max [abs [sin an]] [abs [cos an]]])]
-        [trce "scale" fac]
-        [set! fac [/ fac 2]]
-        [collect-garbage 'incremental]
-        [let [(scale
-          [matrix [[fac 0.0 0.0 0.0]
-                   [0.0 fac 0.0 0.0]
-                   [0.0 0.0 fac 0.0]
-                   [0.0 0.0 0.0 1.0]]])]
-          [let [(result [matrix* [matrix-transpose translate] [matrix-transpose scale]])]
-            [let [(converted [list->f32vector [matrix->list result]])]
-              [glUniformMatrix4fv mvp 1 #f converted]
-              [glEnableVertexAttribArray 0]
-              [glBindBuffer GL_ARRAY_BUFFER [VertexBuffer-id shape]]
-              [let ([index 0]
-                    [size 3]
-                    [type GL_FLOAT]
-                    [normalized #f]
-                    [stride 0 #;[had it at 12]]
-                    [offset 0])
-                [glVertexAttribPointer index size #;[size = number of components per vertex] type normalized stride offset]
-                [glDrawArrays GL_TRIANGLES 0 [VertexBuffer-vertex-count shape]]
-                [glDisableVertexAttribArray 0]]]]]]]]]
-[define my-canvas% [class* canvas% []
-  [inherit
-    refresh
-    with-gl-context
-    swap-gl-buffers]
-  [define stop-drawing #f]
-  [define/override [on-paint]
-    [with-gl-context
-      [lambda []
-        [parameterize ([trce-enabled #f])
-          [draw-opengl]]
-        [swap-gl-buffers]]]
-    [sleep/yield 0.01]
-    [when [not stop-drawing]
-      [on-paint]]]
-  [define/override [on-size width height]
-    [with-gl-context
-      [lambda []
-        [resize width height]]]]
-  [define/override [on-event event]
-    [begin-busy-cursor]
-    [on-paint]
-    [info event]]
-  [define/override [on-superwindow-show state]
-    [trce 'on-close state]
-    [set! stop-drawing [not state]]]
-  [define/override [on-char event]
-    [warn event]]
-  [super-instantiate [] [style '[gl]]]]]
-[define win [new frame% [label "Collision Testing"] [min-width 400] [min-height 400]]]
-[define gl [new my-canvas% [parent win]]]
-[define [setup-gl]
-  [glEnable GL_DEPTH_TEST]
-  [glClearDepth 1.0]
-  [glDepthFunc GL_LEQUAL]
-  [info "we're here"]
-  [define vertex-buffer [create-vertex-buffer '[
-     -1.0 -1.0 1.0
-      1.0 -1.0 1.0
-      0.0  1.0 1.0
-
-      0.0  0.0 1.0
-      2.0  0.0 1.0
-      1.0  2.0 1.0
-
-     -1.0 -2.0 1.0
-      1.0 -2.0 1.0
-      0.0  0.0 1.0]]]
-  [info "here now"]
-  [define v-shader [make-shader "vertex.glsl" GL_VERTEX_SHADER]]
-  [define f-shader [make-shader "fragment.glsl" GL_FRAGMENT_SHADER]]
-  [define program [glCreateProgram]]
-  [glAttachShader program v-shader]
-  [glAttachShader program f-shader]
-  [glLinkProgram program]
-  [define link-status [glGetProgramiv program GL_LINK_STATUS]]
-  [define p-log-length [glGetProgramiv program GL_INFO_LOG_LENGTH]]
-  [when [> p-log-length 0]
-    [define-values [count bytes] [glGetShaderInfoLog program p-log-length]]
-    [crit "Shader failed to compile" bytes]]
-  [glDetachShader program v-shader]
-  [glDetachShader program f-shader]
-  [glDeleteShader v-shader]
-  [glDeleteShader f-shader]
-  [glUseProgram program]
-  [values program vertex-buffer]]
-[define-values [program shape] [send gl with-gl-context setup-gl]]
-
-[send win show #t]
+[let-values ([(program window) [new-game-window]])
+  [trce "Initializing main state"]
+  [let init ([continue? #t]
+             [character [send window with-gl-context [lambda () [create-vertex-buffer mychar GL_TRIANGLE_FAN]]]]
+             [footing [send window with-gl-context [lambda () [create-vertex-buffer footholds GL_LINE_STRIP]]]]
+             [monsters #f]
+             [footholds empty])
+    [trce "Initialized main state finished OK"]
+    [let loop ([last-time [current-inexact-milliseconds]]
+               [an 0.0]
+               [x-pos 0]
+               [y-pos 0.5]
+               [y-speed 0.0]
+               [current-fh empty])
+      [logic]
+      [let* ([x-pos [+ x-pos [if [send window key-down? #\a] -0.01 [if [send window key-down? #\d] 0.01 0.0]]]]
+             [y-speed [if [send window key-down? #\w] 0.05 0.0]]
+             [current-fh [if [send window key-down? #\w] empty current-fh]]
+             [y-pos [+ y-pos y-speed]]
+             [collision [resolve-y x-pos y-pos [- y-pos 0.01] current-fh point-footholds]]
+             [y-pos [car collision]]
+             [current-fh [cadr collision]])
+        [draw]
+        [when continue?
+          [loop [current-inexact-milliseconds]
+                an
+                x-pos
+                y-pos
+                y-speed
+                current-fh]]]]]]
