9fd292c78439e3bb1da695869b15e93c098595bd
Kevin Robert Stravers <macocio@gmail.com>
2017-01-07 23:25:22 +0100

    Implement a simple server

--- web.rkt
+++ web.rkt
@@ -1,23 +1,44 @@
-#lang racket
+#lang racket/base
 
-[require web-server/servlet web-server/servlet-env]
 
-[define (start req)
-  [displayln req]
-  [response/xexpr
-    `(html (head (title "Hello world"))
-           (body (h1 "Wow guys it's amazing!")))]]
+(for/list ([f (in-directory "./posts")]) ; #:when (regexp-match? "\\.rkt$" f))
+     [path->string [file-name-from-path f]])
 
-[serve/servlet start #:port 8081 #:servlet-path "/main"]
-
-[for/list ([i [for/stream ([i [range 0 1 1/5]]) [sleep i] i]])
-  i]
-
-[stream-nth [stream [sleep 1] [begin [sleep 2] [displayln "HEY"]]] 1]
-
-[define (stream-nth stream index)
-  [cond
-    [(stream-empty? stream) empty]
-    [(= index 0) [stream-first stream]]
-    [else [stream-nth [stream-rest stream] [sub1 index]]]]]
-[stream-nth [stream-map [lambda (x) [expt x 3]] [in-naturals]] 3]
+(require (prefix-in dispatch: web-server/dispatch)
+         (prefix-in dispatch-log: web-server/dispatchers/dispatch-log)
+         (prefix-in xexpr: web-server/http/xexpr)
+         (prefix-in servlet: web-server/servlet-env))
+(define-values (route-dispatch route-url)
+  (dispatch:dispatch-rules
+    [("") home-route]
+    [("info") info-route]
+    [else not-found-route]))
+(define (not-found-route request)
+  (xexpr:response/xexpr
+    `(html (body (h2 "Uh-oh! Page not found.")))))
+(define (home-route request)
+  (xexpr:response/xexpr
+    `(html (body
+      (h2 "Why state is harmful")
+      (h2 "Look ma, no state!!!!!!!!!")
+      (h2 "Another big guy (for u)")
+      ))))
+(define (info-route request)
+  (xexpr:response/xexpr
+    `(html
+      (body
+        (h2 "This is an information page")
+        (hr)
+        (p "Information will be added here periodically")
+        (a ((href ,(route-url home-route))) "Home")
+        (p "There are currently no individuals who would like to engage in armed conflicts")
+        ))))
+(define (route-dispatch/log-middleware req)
+  (display (dispatch-log:apache-default-format req))
+  (flush-output)
+  (route-dispatch req))
+(servlet:serve/servlet
+  route-dispatch/log-middleware
+  #:servlet-path "/"
+  #:servlet-regexp #rx""
+  #:stateless? #t)
